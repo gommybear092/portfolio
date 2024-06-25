4cf6e15be5992a84fb0a7ef6ab3677c888b30ee6
gommybear092 <gommybear092@gmail.com>
2024-06-25 15:17:19 +0900

    section 1, 2 (ing)

---
+++ js/common.js
@@ -0,0 +1,37 @@
+    // Event
+    loadHandler();
+    $(window).on("scroll", scrollHandler);
+
+    // Func : loadHandler
+    function loadHandler() {
+        let t = $(this).scrollTop();
+        floating(t);
+        desktop_top_button(t);
+    }
+
+    // Func : Scroll
+    function scrollHandler() {
+        let t = $(this).scrollTop();
+        floating(t);
+        desktop_top_button(t);
+    }
+
+    // Func : floating
+    function floating(t) {
+        (t > 50) ? $("#header").addClass("floating") : $("#header").removeClass("floating");
+        return;
+    }
+
+    // Func : desktop_top_button
+    function desktop_top_button(t) {
+        let ww_t = window.visualViewport.height;
+        let foo_h = $("#footer").innerHeight();
+        (t > 550) ? $("#btn-desktop-top").addClass("show") : $("#btn-desktop-top").removeClass("show");
+
+        if ((Math.round(t) + Math.round(ww_t)) >= $(document).height() - foo_h) {
+            $("#btn-desktop-top").addClass("__footer-bottom");
+        } else {
+            $("#btn-desktop-top").removeClass("__footer-bottom");
+        }
+        return;
+    }
